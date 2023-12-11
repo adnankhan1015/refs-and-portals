440277c45af28ead31de5c02b05cea5a9f48717b
Adnan Khan <adnankhannad@gmail.com>
2023-12-11 23:55:55 +0500

    Add two way binding on the field

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [eneteredPlayerName, setEneteredPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  function handleChange(event) {
+    setSubmitted(false);
+    setEneteredPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? eneteredPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={eneteredPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
---
+++ src/understandings/refs-and-portals.md
@@ -0,0 +1,14 @@
+### Working Refs and portals
+
+#### Refs
+
+- Advanced DOM Access and value management.
+- Use this refs feature to gain simplified direct access to DOM element and what you can do with that access.
+- Use Refs feature to manage valus that shouldn't be state.
+- You will also learn how you can build components that expose and API.
+- Exposing API functions from components. for example callable functions to other components with help of refs concept.
+
+#### Portals
+
+- Detaching DOM rendering from JSX structure with portals.
+- How we can use this portal feature to detach the place where an element gets rendered in the actual DOM from the place where you use it in your JSX code.
