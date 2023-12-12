4a14d5d43f2f34c8420d10fbe4ad74aa8a2f357e
Adnan Khan <adnankhannad@gmail.com>
2023-12-12 23:01:52 +0500

    Connect input to refs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,22 +1,20 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
-  const [eneteredPlayerName, setEneteredPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-  function handleChange(event) {
-    setSubmitted(false);
-    setEneteredPlayerName(event.target.value);
-  }
+  const playerName = useRef();
+
+  const [eneteredPlayerName, setEneteredPlayerName] = useState(null);
 
   function handleClick() {
-    setSubmitted(true);
+    setEneteredPlayerName(playerName.current.value);
+    playerName.current.value = "";
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? eneteredPlayerName : "unknown entity"}</h2>
+      <h2>Welcome {eneteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={eneteredPlayerName} />
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
---
+++ src/understandings/01-Intro-to-Refs.md
@@ -0,0 +1,7 @@
+### Refs
+
+#### Connecting and Access HTML elements via refs
+
+- Q. What are Refs? and How can we simplify this(Player.jsx) component with Refs?
+
+- A ref in a React is just a value, just as the state in the end is a value and just as a variable contains a value. BUT its a special kind of value. or It is managed by react in a special way to be precise.
